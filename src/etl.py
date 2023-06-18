aef16e31e26f48092cf46d081ed6eb2d62a8b4a6
DavidIpsos <david.solano@ipsos.com>
2023-06-18 12:05:36 +0200

    modify modules with experiment structure

--- src/etl.py
+++ src/etl.py
@@ -3,7 +3,14 @@ from data.data_preprocessing import preprocess_data
 from data.data_splitting import split_data
 
 
-def etl(num_instances: int, test_size: float):
-    collect_data(num_instances=num_instances, output_dir="bronze")
-    preprocess_data(input_dir="bronze", output_dir="silver")
-    split_data(input_dir="silver", output_dir="golden", test_size=test_size)
+def etl(model_name, num_instances: int, test_size: float):
+    collect_data(
+        model_name=model_name, num_instances=num_instances, output_dir="bronze"
+    )
+    preprocess_data(model_name=model_name, input_dir="bronze", output_dir="silver")
+    split_data(
+        model_name=model_name,
+        input_dir="silver",
+        output_dir="golden",
+        test_size=test_size,
+    )
